2cb9156b4b52f056a69e7e8d8f7ef98844a2fe18
Toocanzs <dupanzszs@gmail.com>
2021-07-06 00:20:53 -0400

    Add built.bat

---
+++ build.bat
@@ -0,0 +1,2 @@
+powershell Compress-Archive -LiteralPath included_fonts, __init__.py, config.json, config.md, manifest.json, README.md -DestinationPath Japanese-Card-Randomizer.zip -Force
+rename Japanese-Card-Randomizer.zip "Japanese Card Randomizer.ankiaddon"
