c5a728bc6fdf5093c2aa14f3d7d5a8713f8e6b16
Toocanzs <dupanzszs@gmail.com>
2021-07-07 03:59:41 -0400

    fix build command

--- build.bat
+++ build.bat
@@ -1,2 +1,6 @@
+del /f "Japanese Card Randomizer.ankiaddon"
+if not exist "included_fonts" ( 
+    mkdir "included_fonts"
+)
 powershell Compress-Archive -LiteralPath included_fonts, __init__.py, config.json, config.md, manifest.json, README.md -DestinationPath Japanese-Card-Randomizer.zip -Force
 rename Japanese-Card-Randomizer.zip "Japanese Card Randomizer.ankiaddon"
